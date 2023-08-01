38a7e60890f0d3f68e11aa82c500ab8375fc9460
Vitalii-Hlushchenko <vitalikglush@gmail.com>
2023-08-01 12:14:23 +0300

    conect material ui

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,6 +1,9 @@
 import { signInWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
 import { auth } from "../firebase";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 import { enqueueSnackbar } from 'notistack'
 
 const SignIn = () => {
@@ -11,7 +14,8 @@ const SignIn = () => {
     e.preventDefault();
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        console.log(userCredential);
+        console.log(userCredential)
+        enqueueSnackbar('sign in successful');
       })
       .catch((error) => {
         console.log(error);
@@ -19,14 +23,40 @@ const SignIn = () => {
   };
 
   return (
-    <div className="sign-in-container">
-      <form className="authForm" onSubmit={signIn}>
-        <h1>Log In to your Account</h1>
-        <input  type="email" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
-        <input type="password" placeholder="Enter your password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
-        <button onClick={() => enqueueSnackbar('sign in successful')} type="submit">Log In</button>
-      </form>
-    </div>
+    // <div className="sign-in-container">
+    //   <form className="authForm" onSubmit={signIn}>
+    //     <h1>Log In to your Account</h1>
+    //     <input  type="email" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
+    //     <input type="password" placeholder="Enter your password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
+    //     <button onClick={() => enqueueSnackbar('sign in successful')} type="submit">Log In</button>
+    //   </form>
+    // </div>
+
+    <Box  component="form" onSubmit={signIn}
+    sx={{'& .MuiTextField-root': { m: 1, width: '25ch' },}} noValidate autoComplete="off">
+      <div class="BoxElement">
+       <h1>Login</h1>
+
+      <TextField
+          required
+          id="outlined-required"
+          label="Enter your email"
+          defaultValue="email"
+          value={email} onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          required
+          id="outlined-required"
+          label="Enter your password"
+          defaultValue="password"
+          value={password} onChange={(e) => setPassword(e.target.value)}        />
+
+      <Button className="BoxBtn" variant="contained" type="submit" size="small">Login</Button>
+
+      </div>
+
+
+    </Box>
   );
 };
 
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,6 +1,9 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
 import { auth } from "../firebase";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 const SignUp = () => {
   const [email, setEmail] = useState("");
@@ -18,14 +21,32 @@ const SignUp = () => {
   };
 
   return (
-    <div className="sign-in-container">
-      <form className="authForm" onSubmit={signUp}>
-        <h1>Create account</h1>
-        <input type="email" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)} ></input>
-        <input type="password" placeholder="Enter your password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
-        <button type="submit">Sign Up</button>
-      </form>
-    </div>
+    
+    <Box  component="form" onSubmit={signUp}
+    sx={{'& .MuiTextField-root': { m: 1, width: '25ch' },}} noValidate autoComplete="off">
+      <div class="BoxElement">
+       <h1>Create new account</h1>
+
+      <TextField
+          required
+          id="outlined-required"
+          label="Enter your email"
+          defaultValue="email"
+          value={email} onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          required
+          id="outlined-required"
+          label="Enter your password"
+          defaultValue="password"
+          value={password} onChange={(e) => setPassword(e.target.value)}        />
+
+      <Button className="BoxBtn" variant="contained" type="submit" size="small">SignUp</Button>
+
+      </div>
+
+
+    </Box>
   );
 };
 
